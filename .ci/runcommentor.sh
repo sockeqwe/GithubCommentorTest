277d3bff7d58cfb4465cfac206e285750d3fb329
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-17 17:42:54 +0100

    More logging

--- .ci/runcommentor.sh
+++ .ci/runcommentor.sh
@@ -1,8 +1,11 @@
+echo "CI_PULL_REQUEST = $CI_PULL_REQUEST"
+echo "CI_PULL_REQUEST = $CIRCLE_PULL_REQUESTS"
+echo "CIRCLE_PR_NUMBER = $CIRCLE_PR_NUMBER"
+
 if [ "$CI_PULL_REQUEST" = "" ]
 then
    echo "No pull request. Therefore skipping Github Commentor"
 else
-   echo $CI_PULL_REQUEST
    java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $CI_PULL_REQUEST -sha $CIRCLE_SHA1 -accesstoken $ACCESS_TOKEN
 fi
 
