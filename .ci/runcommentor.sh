a595c1a77930701b8d771588651b64771e268f5e
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-17 11:42:42 +0100

    Check for master / pull request

---
+++ .ci/runcommentor.sh
@@ -0,0 +1,7 @@
+if [ "CI_PULL_REQUEST" = "" ]
+then
+   echo "No pull request. Therefore skipping Github Commentor"
+else
+   java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $CI_PULL_REQUEST -sha $CIRCLE_SHA1 -accessToken $token
+fi
+
