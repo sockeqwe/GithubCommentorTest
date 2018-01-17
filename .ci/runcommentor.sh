68f3ed53bdb70735e498d25c4eb1843777dde326
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-17 18:11:04 +0100

    Try .travis

--- .ci/runcommentor.sh
+++ .ci/runcommentor.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+sha=`git rev-parse HEAD`
+echo "sha = $sha"
+
 echo "CI_PULL_REQUEST = $CI_PULL_REQUEST"
 echo "CI_PULL_REQUEST = $CIRCLE_PULL_REQUESTS"
 echo "CIRCLE_PR_NUMBER = $CIRCLE_PR_NUMBER"
 echo "CIRCLE_BRANCH = $CIRCLE_BRANCH"
 
-if [ "$CIRCLE_BRANCH" = "master" ]
+echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
+echo "TRAVIS_PULL_REQUEST_BRANCH = $TRAVIS_PULL_REQUEST_BRANCH"
+echo "TRAVIS_PULL_REQUEST_SHA = $TRAVIS_PULL_REQUEST_SHA"
+echo "TRAVIS_PULL_REQUEST_SLUG = $TRAVIS_PULL_REQUEST_SLUG"
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
    echo "No pull request. Therefore skipping Github Commentor"
 else
-   java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $CI_PULL_REQUEST -sha $CIRCLE_SHA1 -accesstoken $ACCESS_TOKEN
+   java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $TRAVIS_PULL_REQUEST -sha $sha -accesstoken $ACCESS_TOKEN
 fi
 
