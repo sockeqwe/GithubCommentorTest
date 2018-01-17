556c388ea0d43312da9ec185909a43c5b7d3114d
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-17 17:35:32 +0100

    Updated accesstoken parameter

--- .ci/runcommentor.sh
+++ .ci/runcommentor.sh
@@ -3,6 +3,6 @@ then
    echo "No pull request. Therefore skipping Github Commentor"
 else
    echo $CI_PULL_REQUEST
-   java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $CI_PULL_REQUEST -sha $CIRCLE_SHA1 -accessToken $ACCESS_TOKEN
+   java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $CI_PULL_REQUEST -sha $CIRCLE_SHA1 -accesstoken $ACCESS_TOKEN
 fi
 
