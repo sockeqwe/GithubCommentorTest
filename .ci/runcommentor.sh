7e9420dfdc232c128e7bfcc88d40bb5ba18fa3be
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-17 17:54:00 +0100

    jdk9 image

--- .ci/runcommentor.sh
+++ .ci/runcommentor.sh
@@ -3,7 +3,7 @@ echo "CI_PULL_REQUEST = $CIRCLE_PULL_REQUESTS"
 echo "CIRCLE_PR_NUMBER = $CIRCLE_PR_NUMBER"
 echo "CIRCLE_BRANCH = $CIRCLE_BRANCH"
 
-if [ "$CI_PULL_REQUEST" = "" ]
+if [ "$CIRCLE_BRANCH" = "master" ]
 then
    echo "No pull request. Therefore skipping Github Commentor"
 else
