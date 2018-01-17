b26ddf810ff1a6cccd92bc744c5020acb36ba4e8
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-17 11:50:51 +0100

    Changed runcommentator.sh

--- .ci/runcommentor.sh
+++ .ci/runcommentor.sh
@@ -1,7 +1,8 @@
-if [ "CI_PULL_REQUEST" = "" ]
+if [ "$CI_PULL_REQUEST" = "" ]
 then
    echo "No pull request. Therefore skipping Github Commentor"
 else
+   echo $CI_PULL_REQUEST"
    java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $CI_PULL_REQUEST -sha $CIRCLE_SHA1 -accessToken $token
 fi
 
