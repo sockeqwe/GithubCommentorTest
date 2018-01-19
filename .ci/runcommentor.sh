3a87173d7ac433ba6f548aefd1a5c8656660cb3b
sockeqwe <hannes.dorfmann@gmail.com>
2018-01-19 10:26:18 +0100

    shell script clean up

--- .ci/runcommentor.sh
+++ .ci/runcommentor.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 sha=`git rev-parse HEAD`
-SLUG="sockeqwe/GithubCommentorTest"
-JDK="oraclejdk8"
-BRANCH="master"
 
 echo "sha = $sha"
 
@@ -17,9 +14,8 @@ echo "TRAVIS_PULL_REQUEST_SHA = $TRAVIS_PULL_REQUEST_SHA"
 echo "TRAVIS_PULL_REQUEST_SLUG = $TRAVIS_PULL_REQUEST_SLUG"
 echo "TRAVIS_REPO_SLUG = $TRAVIS_REPO_SLUG" 
 
-# if [ "$TRAVIS_REPO_SLUG" = "$SLUG" ] && ["$TRAVIS_BRANCH" = "$BRANCH"]; then
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ]; then
-   echo "No pull request. Therefore skipping Github Commentor"
+   echo "No pull request.  Skipping Github Commentor ..."
 else
    java -jar .ci/githubcommentor.jar -file .ci/comments.xml -owner sockeqwe -repository GithubCommentorTest -pullrequest $TRAVIS_PULL_REQUEST -sha $TRAVIS_PULL_REQUEST_SHA -accesstoken $ACCESS_TOKEN
 fi
